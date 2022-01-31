5bf7a3a43d0260b0983b4e7e0c9d3706f8c3d5b0
hasnain003 <mdhasnain839@gmail.com>
2022-01-31 20:24:03 +0530

    Added Word Count Reducer

--- src/main/java/Assignment2/part1/WCReducer.java
+++ src/main/java/Assignment2/part1/WCReducer.java
@@ -1,2 +1,19 @@
-package Assignment2.part1;public class WCReducer {
+package Assignment2.part1;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WCReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    //private IntWritable result = new IntWritable();
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException,InterruptedException {
+        int sum = 0;
+        for(IntWritable val: values) {
+            sum += val.get();
+        }
+        //result.set(sum);
+        context.write(key,new IntWritable(sum));
+    }
 }
