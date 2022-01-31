acfb3aced9ba50d2dc07588c6fa153167b660c4e
hasnain003 <mdhasnain839@gmail.com>
2022-01-31 20:19:40 +0530

    Restructured the class

--- src/main/java/Assignment1/services/UploadToHDFS.java
+++ src/main/java/Assignment1/services/UploadToHDFS.java
@@ -1,4 +1,4 @@
-package services;
+package Assignment1.services;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -8,36 +8,34 @@ import java.io.File;
 import java.net.URI;
 
 public class UploadToHDFS {
-    //public static final String HDFSNAME = "fs.defaultFS";
-    //public static final String HDFSPATH = "hdfs://localhost:9000/";
+    public static final String HDFSNAME = "fs.defaultFS";
+    public static final String HDFSPATH = "hdfs://localhost:9000/";
     public static void main(String args[]) throws Exception {
-        //System.out.println("Uploading to hdfs started");
-        //String path = "/Users/mdhasnain/FileStore/";
+        System.out.println("Uploading to hdfs started");
+        String path = "/Users/mdhasnain/FileStore/";
         String uri = "hdfs://localhost:9000/";
 
 
-       // File directoryPath = new File(path);
-        //String contents[] = directoryPath.list();
-        //System.out.println(contents.length);
+        File directoryPath = new File(path);
+        String contents[] = directoryPath.list();
+        System.out.println(contents.length);
 
-//        int i = 1;
-//
-        for(int i = 1;i<=100;i++) {
-            String localpath="/Users/mdhasnain/FileStore/people"+Integer.toString(i)+".csv";
-            String dir = "hdfs://localhost:9000/Users/mdhasnain/Assisgnemt/";
-            //System.out.println(fileName);
-            Configuration configuration = new Configuration();
-            FileSystem fileSystem = FileSystem.get(URI.create(uri),configuration);
-            fileSystem.copyFromLocalFile(new Path(localpath),new Path(dir));
+        Configuration configuration = new Configuration();
+        configuration.set(HDFSNAME,HDFSPATH);
+        configuration.addResource(new Path("/HADOOP_HOME/conf/core-site.xml"));
+        configuration.addResource(new Path("/HADOOP_HOME/conf/hdfs-site.xml"));
+        FileSystem fileSystem = FileSystem.get(new URI(HDFSPATH),configuration);
+        String dir = "hdfs://localhost:9000/Users/mdhasnain/Test/";
+        Path path1 = new Path(dir);
+        fileSystem.mkdirs(path1);
+       for(String fileName: contents) {
+
+            System.out.println(fileName);
+
+
+            fileSystem.copyFromLocalFile(new Path(path+fileName),path1);
         }
-//       for(String fileName: contents) {
-//           String dir = "hdfs://localhost:9000/Users/mdhasnain/Assisgnemt/";
-//           //System.out.println(fileName);
-//            Configuration configuration = new Configuration();
-//            FileSystem fileSystem = FileSystem.get(new URI(HDFSPATH),configuration);
-//            fileSystem.copyFromLocalFile(new Path(path+fileName),new Path(dir));
-//        }
         System.out.println("Uploading to HDFS completed");
-        //return fileSystem;
+
     }
 }
