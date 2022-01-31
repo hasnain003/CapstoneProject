0f9acd071920f661fedb1623292c3c2710537a50
hasnain003 <mdhasnain839@gmail.com>
2022-01-31 20:42:29 +0530

    Added Employee Model

--- src/main/java/Assignment3/model/Employee.java
+++ src/main/java/Assignment3/model/Employee.java
@@ -1,2 +1,58 @@
-package Assignment3.model;public class Employee {
+package Assignment3.model;
+
+public class Employee {
+    private String name;
+    private String employee_id;
+    private String building_code;
+    private Integer floor_number;
+    private Integer salary;
+    private String department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(String employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getBuilding_code() {
+        return building_code;
+    }
+
+    public void setBuilding_code(String building_code) {
+        this.building_code = building_code;
+    }
+
+    public Integer getFloor_number() {
+        return floor_number;
+    }
+
+    public void setFloor_number(Integer floor_number) {
+        this.floor_number = floor_number;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
