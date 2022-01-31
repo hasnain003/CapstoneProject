4723f28641fef24f7bf15801cfcf683418f7ad1b
hasnain003 <mdhasnain839@gmail.com>
2022-01-31 20:41:18 +0530

    Added Building Model

--- src/main/java/Assignment3/model/Building.java
+++ src/main/java/Assignment3/model/Building.java
@@ -1,2 +1,40 @@
-package Assignment3.model;public class Building {
+package Assignment3.model;
+
+public class Building {
+    private String buildingCode;
+    private Integer totalFloors;
+    private Integer companiesInBuilding;
+    private String cafeteriaCode;
+
+    public String getBuildingCode() {
+        return buildingCode;
+    }
+
+    public void setBuildingCode(String buildingCode) {
+        this.buildingCode = buildingCode;
+    }
+
+    public Integer getTotalFloors() {
+        return totalFloors;
+    }
+
+    public void setTotalFloors(Integer totalFloors) {
+        this.totalFloors = totalFloors;
+    }
+
+    public Integer getCompaniesInBuilding() {
+        return companiesInBuilding;
+    }
+
+    public void setCompaniesInBuilding(Integer companiesInBuilding) {
+        this.companiesInBuilding = companiesInBuilding;
+    }
+
+    public String getCafeteriaCode() {
+        return cafeteriaCode;
+    }
+
+    public void setCafeteriaCode(String cafeteriaCode) {
+        this.cafeteriaCode = cafeteriaCode;
+    }
 }
