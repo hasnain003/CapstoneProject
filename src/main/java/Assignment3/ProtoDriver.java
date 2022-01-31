6f90f0d9c700eceb3e1b10964867c29c312c49be
hasnain003 <mdhasnain839@gmail.com>
2022-01-31 20:40:15 +0530

    Added Printing method for Building and Employee proto

--- src/main/java/Assignment3/ProtoDriver.java
+++ src/main/java/Assignment3/ProtoDriver.java
@@ -1,2 +1,61 @@
-package Assignment3;public class ProtoDriver {
+package Assignment3;
+
+
+import example.employees.EmployeeOuterClass;
+import example.employees.EmployeeOuterClass.Employee;
+import example.buildings.BuildingOuterClass.Building;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ProtoDriver {
+    public static final String CSV_FILE_PATH = "/Users/mdhasnain/FileStore/";
+    public static void printEmployeesProto() throws IOException {
+        FileReader fileReader = new FileReader(CSV_FILE_PATH+"employee.csv");
+        BufferedReader br = new BufferedReader(fileReader);
+        String line;
+        br.readLine();
+        while((line = br.readLine()) != null) {
+            String[] fields = line.split(",");
+            System.out.println(fields[3]);
+            Employee.Builder builder = Employee.newBuilder();
+            builder.setName(fields[0].replaceAll("\"", ""))
+                    .setEmployeeId(fields[1].replaceAll("\"", ""))
+                    .setBuildingCode(fields[2].replaceAll("\"", ""))
+                    .setFloorNumber(EmployeeOuterClass.Floor_Number.forNumber(Integer.parseInt(fields[3].replaceAll("\"", ""))))
+                    .setSalary(Integer.parseInt(fields[4].replaceAll("\"", "")))
+                    .setDepartment(fields[5].replaceAll("\"", ""));
+            System.out.println(builder.toString());
+        }
+        fileReader.close();
+    }
+    public static void printBuildingProto() throws IOException {
+        System.out.println("started.....");
+        FileReader fileReader = new FileReader(CSV_FILE_PATH+"building.csv");
+        System.out.println("Opened the csv files....");
+        BufferedReader br = new BufferedReader(fileReader);
+        String line;
+        System.out.println("Started reading the CSV files");
+        br.readLine();
+        while((line = br.readLine()) != null) {
+            String[] fields = line.split(",");
+
+            Building.Builder builder = Building.newBuilder();
+            builder.setBuildingCode(fields[0].replaceAll("\"", ""))
+                    .setTotalFloors(Integer.parseInt(fields[1].replaceAll("\"", "")))
+                    .setCompaniesInTheBuilding(Integer.parseInt(fields[2].replaceAll("\"", "")))
+                    .setCafeteriaCode(fields[3].replaceAll("\"", ""));
+            System.out.println(builder.toString());
+        }
+        System.out.println("Finished printing proto objects");
+        fileReader.close();
+        System.out.println("Closed the File");
+    }
+
+    public static void main(String[] args) throws IOException {
+        printBuildingProto();
+        printEmployeesProto();
+    }
 }
